d133f618ffca497f4ef2d34f8eb78c215e467f03
Yuvaraja D <yuvraj3303@gmail.com>
2023-06-13 12:23:09 +0530

    third change

--- question_model.py
+++ question_model.py
@@ -1,5 +1,5 @@
 class Question:
     
-    def __init__(self,dtext,question_ans):
-        self.text=dtext
+    def __init__(self,q_text,question_ans):
+        self.text=q_text
         self.answer=question_ans
